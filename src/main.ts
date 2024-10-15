a715d9b976cc619e62cdd41ca4dbe7b02df3c2c5
Calvin Hunter <calhunte@ucsc.edu>
2024-10-14 20:14:04 -0700

    Changed App Name
    I changed the app name from hello
    to This is a typescript app

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import "./style.css";
 
-const APP_NAME = "Hello";
+const APP_NAME = "This is a Type script app!";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
