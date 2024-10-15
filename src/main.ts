c72aea727bf09c96e3e97bc7ff7928b0f9b2d829
Calvin Hunter <calhunte@ucsc.edu>
2024-10-15 15:28:05 -0700

    Step.2 Complete
    I added a clear button as well as functionality to draw on the canvas using mousedown and up and mouse functions

--- src/main.ts
+++ src/main.ts
@@ -9,14 +9,80 @@ const header = document.createElement("h1");
 header.innerHTML = APP_NAME;
 app.append(header);
 
-const canvas = document.createElement("canvas");
+//Clear button
+const clearButtonName = "Clear";
+const clearButton = document.createElement("button");
+clearButton.innerHTML = clearButtonName;
+app.append(clearButton);
 
+//spacing
+app.append(document.createElement("div"));
+
+//Canvas!
+const canvas = document.createElement("canvas");
 canvas.width = 256;
 canvas.height = 256;
-
 app.append(canvas);
 
-const ctx = canvas.getContext("2d");
+//Get context
+const context = canvas.getContext("2d");
 
-ctx.fillStyle = "skyblue";
-ctx.fillRect(0, 0, 256, 256);
+console.log(typeof context);
+
+//make it blue and such
+context!.fillStyle = "skyblue";
+context!.fillRect(0, 0, 256, 256);
+
+//Drawing vars
+let isDrawing = false;
+let x = 0;
+let y = 0;
+
+//On mouse down inside canvas
+canvas.addEventListener("mousedown", (e) => {
+  x = e.offsetX;
+  y = e.offsetY;
+  isDrawing = true;
+});
+
+//on mouse move inside canvas
+canvas.addEventListener("mousemove", (e) => {
+  if (isDrawing) {
+    drawLine(context!, x, y, e.offsetX, e.offsetY);
+    x = e.offsetX;
+    y = e.offsetY;
+  }
+});
+
+//on mouse up on on canvas
+window.addEventListener("mouseup", (e) => {
+  if (isDrawing) {
+    drawLine(context!, x, y, e.offsetX, e.offsetY);
+    x = 0;
+    y = 0;
+    isDrawing = false;
+  }
+});
+
+//funciton to draw line, idk what type context is..
+function drawLine(
+  context: CanvasRenderingContext2D,
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number
+) {
+  context.beginPath();
+  context.strokeStyle = "black";
+  context.lineWidth = 1;
+  context.moveTo(x1, y1);
+  context.lineTo(x2, y2);
+  context.stroke();
+  context.closePath();
+}
+
+clearButton.addEventListener("click", () => {
+  context!.clearRect(0, 0, canvas.width, canvas.height);
+  context!.fillStyle = "skyblue";
+  context!.fillRect(0, 0, 256, 256);
+});
