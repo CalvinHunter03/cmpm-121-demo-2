4d5424752078782d1a7b7e082db718d5ed7c1bde
Calvin Hunter <calhunte@ucsc.edu>
2024-10-15 13:00:14 -0700

    Step 1 complete
    I added a border and a border radius and I added a lil drop shadow for the fans.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,22 @@
 import "./style.css";
 
-const APP_NAME = "This is a Type script app!";
+const APP_NAME = "Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+
+const header = document.createElement("h1");
+header.innerHTML = APP_NAME;
+app.append(header);
+
+const canvas = document.createElement("canvas");
+
+canvas.width = 256;
+canvas.height = 256;
+
+app.append(canvas);
+
+const ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "skyblue";
+ctx.fillRect(0, 0, 256, 256);
